91d28b86580dfe94bb4f4f22e0e4d5ca9b1bd492
Connor <Woodconnor13@gmail.com>
2022-08-01 15:56:26 -0400

    finished JS countries lab

--- country.js
+++ country.js
@@ -5,14 +5,24 @@ class Country {
         this.greeting = greeting;
         this.colors = colors;
     }
+    ChangeColor(){
+        document.querySelector("#Color1").style.backgroundColor = this.colors[0];
+        document.querySelector("#Color2").style.backgroundColor = this.colors[1];
+        document.querySelector("#Color3").style.backgroundColor = this.colors[2];
+        document.querySelector("#CountryName").innerText = `${this.name}`;
+        document.querySelector("#OfficialLanguage").innerText = `${this.lang}`;
+        document.querySelector("#HelloWorld").innerText = `${this.greeting}`;
+    }
 }
 let usa = new Country("USA", "Murican", "Why Hello there world! Have some McDonalds", ["red", "white", "blue"]);
 let mexico = new Country("Mexico", "Spanish", "Hola mundo!", ["red", "white", "green"]);
 let algeria = new Country("Algeria", "Arabic and Berber", "Sup", ["blue", "yellow", "red"])
+let germany = new Country("Germany", "German", "Hallo Welt", ["black", "red", "yellow"])
+let egypt = new Country("Egypt", "Arabic", "مرحبا بالعالم", ["red", "black", "white"]);
 
 
 function SwitchCountry() {
-    let input = "USA";
+    let input = document.querySelector("#CountryList").value;
     let country;
 
     if (input === "USA") {
@@ -25,5 +35,36 @@ function SwitchCountry() {
     else if (input === "Algeria") {
         country = algeria;
     }
-
+    else if (input === "Germany") {
+        country = germany;
+    }
+    else if (input === "Egypt") {
+        country = egypt;
+    }
+    //DisplayColors(country);
+    country.ChangeColor();
 }
+
+// function DisplayColors(country){
+//     document.querySelector("#Color1").style.backgroundColor = country.colors[0];
+//     document.querySelector("#Color2").style.backgroundColor = country.colors[1];
+//     document.querySelector("#Color3").style.backgroundColor = country.colors[2];
+// }
+
+// function DisplayColorsMexico(){
+//     let firstColor = document.querySelector("#Color1");
+//     firstColor.style.backgroundColor = country.colors[0];
+//     let secondColor = document.querySelector("#Color2");
+//     secondColor.style.backgroundColor = country.colors[1];
+//     let thirdColor = document.querySelector("#Color3");
+//     thirdColor.style.backgroundColor = country.colors[2];
+// }
+
+// function DisplayColorsAlgeria(){
+//     let firstColor = document.querySelector("#Color1");
+//     firstColor.style.backgroundColor = country.colors[0];
+//     let secondColor = document.querySelector("#Color2");
+//     secondColor.style.backgroundColor = country.colors[1];
+//     let thirdColor = document.querySelector("#Color3");
+//     thirdColor.style.backgroundColor = country.colors[2];
+// }
